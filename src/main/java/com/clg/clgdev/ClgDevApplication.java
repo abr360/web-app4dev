0207eaa63dca95ea946dd1b55d85f130aa36a240
ALBRIGHT RAJ S J <136182836+abr360@users.noreply.github.com>
2024-03-16 15:04:42 +0000

    Add files via upload

---
+++ src/main/java/com/clg/clgdev/ClgDevApplication.java
@@ -0,0 +1,13 @@
+package com.clg.clgdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClgDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClgDevApplication.class, args);
+	}
+
+}
